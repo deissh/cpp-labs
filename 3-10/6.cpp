12fd7451f5c2e1085d3247190aa2a64d07ef206b
deissh <deissh@yandex.ru>
2021-06-06 20:50:35 +0300

    feat: 3-10/6

--- 3-10/6.cpp
+++ 3-10/6.cpp
@@ -5,16 +5,36 @@
 Сравнить по реальному времени работы алгоритмы линейного поиска,
 поиска с барьером и двоичного поиска при разной размерности n случайного массива.
 */
+// WARNING
+// для бенчмарков использовал https://github.com/google/benchmark
+// brew install google-benchmark
+// и потом в CMakeLists.txt
+//   target_link_libraries(cpp_labs_3_10__6 benchmark::benchmark)
 
-// warning: я не умею правильно готовить бенчмарки под cpp поэтому вот
-// https://quick-bench.com/q/UqJMbIwGAcsCKdbwezDW-jO839o
+/* output
+/Users/deissh/work/RGATU/cpp-labs/cmake-build-debug/cpp_labs_3_10__6
+2021-06-06T15:17:52+03:00
+Running /Users/deissh/work/RGATU/cpp-labs/cmake-build-debug/cpp_labs_3_10__6
+Run on (4 X 2400 MHz CPU s)
+CPU Caches:
+  L1 Data 32 KiB (x2)
+  L1 Instruction 32 KiB (x2)
+  L2 Unified 256 KiB (x2)
+  L3 Unified 3072 KiB (x1)
+Load Average: 3.72, 3.38, 3.07
+--------------------------------------------------------
+Benchmark              Time             CPU   Iterations
+--------------------------------------------------------
+seekLineBench  139025571 ns    133537000 ns            5
+seekBarBench   118075759 ns    116407000 ns            6
+seekBinRBench   92364744 ns     91705875 ns            8  <- как и ожидалось, самый быстрый алгоритм
+*/
 
-// импорт для бенчмарков
-// https://github.com/google/benchmark
-// можно запустить по ссылке выше
 #include <benchmark/benchmark.h>
 #include <ctime>
 #include <cstdlib>
+
+#define ARR_SIZE 10000000
 
 bool seekLine(int* a, int n, int k, int& ind) {
     ind = 0;
@@ -31,18 +51,6 @@ bool seekBar(int* arr, int n, int k, int& idx) {
     return idx < n;
 }
 
-bool seekBin(const int* arr, int n, int k, int& idx) {
-    int l = 0;
-    int r = (n - 1);
-    do {
-        idx = (l + r) / 2;
-        if (arr[idx] < k) l = ++idx;
-        else r = --idx;
-    } while (l <= r && arr[idx] != k);
-
-    return arr[idx] == k;
-}
-
 bool seekBinR(int* arr, int l, int r, int k, int& idx) {
     idx = (l + r) / 2;
     if (arr[idx] == k) return true;
@@ -53,14 +61,26 @@ bool seekBinR(int* arr, int l, int r, int k, int& idx) {
         return seekBinR(arr, l, idx - 1, k, idx);
 }
 
+void randArr(int* arr, int size) {
+    for (int i = 0; i < size; ++i)
+        arr[i] = rand();
+}
+
 // можно закоментировать что бы скомпилировать локально
 /// BENCHMARKS
 static void seekLineBench(benchmark::State& state) {
-    int arr[5] = {1, 2, 3, 4, 5};
-    int ind;
+    // set random seed
+    std::srand(0); // NOLINT(cert-msc51-cpp)
 
     for (auto _ : state) {
-        seekLine(arr, 5, 5, ind);
+        state.PauseTiming();
+        int* arr = new(int[ARR_SIZE]);
+        int ind;
+        state.ResumeTiming();
+
+        randArr(arr, ARR_SIZE);
+
+        seekLine(arr, ARR_SIZE, 5, ind);
 
         benchmark::DoNotOptimize(ind);
         benchmark::DoNotOptimize(arr);
@@ -69,11 +89,18 @@ static void seekLineBench(benchmark::State& state) {
 BENCHMARK(seekLineBench);
 
 static void seekBarBench(benchmark::State& state) {
-    int arr[5] = {1, 2, 3, 4, 5};
-    int ind;
+    // set random seed
+    std::srand(0); // NOLINT(cert-msc51-cpp)
 
     for (auto _ : state) {
-        seekBar(arr, 5, 5, ind);
+        state.PauseTiming();
+        int* arr = new(int[ARR_SIZE]);
+        int ind;
+        state.ResumeTiming();
+
+        randArr(arr, ARR_SIZE);
+
+        seekBar(arr, ARR_SIZE, 5, ind);
 
         benchmark::DoNotOptimize(ind);
         benchmark::DoNotOptimize(arr);
@@ -81,25 +108,19 @@ static void seekBarBench(benchmark::State& state) {
 }
 BENCHMARK(seekBarBench);
 
-static void seekBinBench(benchmark::State& state) {
-    int arr[5] = {1, 2, 3, 4, 5};
-    int ind;
-
-    for (auto _ : state) {
-        seekBin(arr, 5, 5, ind);
-
-        benchmark::DoNotOptimize(ind);
-        benchmark::DoNotOptimize(arr);
-    }
-}
-BENCHMARK(seekBinBench);
-
 static void seekBinRBench(benchmark::State& state) {
-    int arr[5] = {1, 2, 3, 4, 5};
-    int ind;
+// set random seed
+    std::srand(0); // NOLINT(cert-msc51-cpp)
 
     for (auto _ : state) {
-        seekBinR(arr, 3, 5, 5, ind);
+        state.PauseTiming();
+        int* arr = new(int[ARR_SIZE]);
+        int ind;
+        state.ResumeTiming();
+
+        randArr(arr, ARR_SIZE);
+
+        seekBinR(arr, ARR_SIZE, ARR_SIZE / 2, 5, ind);
 
         benchmark::DoNotOptimize(ind);
         benchmark::DoNotOptimize(arr);
